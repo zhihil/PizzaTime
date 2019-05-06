251ccaff77a254d5332dd9fe6135758dbc2f1bf8
Brennan Lu <zh4lu@uwaterloo.ca>
2019-05-05 22:31:37 -0400

    jumbotron ~ Added beer and chicken photos to Jumbotron

--- src/components/LandingJumbotron/LandingJumbotronComponent.tsx
+++ src/components/LandingJumbotron/LandingJumbotronComponent.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import pizzaImg1 from "../../images/jumbo-pizza-1.jpeg";
+import jumboImg1 from "../../images/jumbo-1.jpeg";
+import jumboImg2 from "../../images/jumbo-2.jpeg";
+import jumboImg3 from "../../images/jumbo-3.jpeg";
 
 export class LandingJumbotronComponent extends React.Component {
     public render() {
         return (
             <Carousel>
                 <Carousel.Item>
-                    <img className="d-block w-100" src={pizzaImg1} alt="" />
+                    <img className="d-block w-100" src={jumboImg1} alt="" />
                     <Carousel.Caption>
                         <h1>It's Pizza Time</h1>
                         <p>
@@ -17,7 +19,7 @@ export class LandingJumbotronComponent extends React.Component {
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className="d-block w-100" src={pizzaImg1} alt="" />
+                    <img className="d-block w-100" src={jumboImg2} alt="" />
                     <Carousel.Caption>
                         <h1>It's Chicken Time</h1>
                         <p>
@@ -27,7 +29,7 @@ export class LandingJumbotronComponent extends React.Component {
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img className="d-block w-100" src={pizzaImg1} alt="" />
+                    <img className="d-block w-100" src={jumboImg3} alt="" />
                     <Carousel.Caption>
                         <h1>It's Beer Time</h1>
                         <p>
