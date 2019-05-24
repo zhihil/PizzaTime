47df6578f7d610213e64960f0bf4747856bcb24b
Brennan Lu <zh4lu@uwaterloo.ca>
2019-05-24 16:34:50 -0400

    Added custom tab title and icon

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Helmet from "react-helmet";
 import { LandingJumbotronComponent } from "../components/LandingJumbotron/LandingJumbotronComponent";
 
 export class OrderPage extends React.Component {
     public render() {
-        return <LandingJumbotronComponent />;
+        return (
+            <>
+                <Helmet>
+                    <title>PizzaTime Home Page</title>
+                </Helmet>
+                <LandingJumbotronComponent />
+            </>
+        );
     }
 }
